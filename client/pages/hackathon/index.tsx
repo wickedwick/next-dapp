fee85a840e0fa4593a219e00abddd31a617c17b6
Travis Wickham <twickham37@gmail.com>
2022-02-03 22:16:18 -0600

    Gray-light background

--- client/pages/hackathon/index.tsx
+++ client/pages/hackathon/index.tsx
@@ -30,15 +30,15 @@ export default function Hackathon() {
       {!content && <h1>Loading...</h1>}
       {content && (
         <section data-aos="fade-up" className={utilStyles.headingMd}>
-          <div className="bg-white text-gray-dark px-5 pt-3 pb-1 mb-3">
+          <div className="bg-gray-light text-gray-dark px-5 pt-3 pb-1 mb-3">
             <h2 className="text-3xl font-medium leading-tight mt-0 mb-5">
               {content.title}
             </h2>
           </div>
-          <div className="bg-white text-gray-dark px-5 pt-2 pb-5">
+          <div className="bg-gray-light text-gray-dark px-5 pt-2 pb-5">
             <div dangerouslySetInnerHTML={{ __html: content.description }} />
           </div>
-          <div className="bg-white text-gray-dark p-5 py-2 mt-5">
+          <div className="bg-gray-light text-gray-dark p-5 py-2 mt-5">
             <div dangerouslySetInnerHTML={{ __html: content.links }} />
           </div>
         </section>
